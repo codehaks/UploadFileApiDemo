505037cb7e3d52e371497cb07b5ad268b014f141
Hakim Ghods <ghods.hakim@hotmail.com>
2022-02-16 16:28:33 +0330

    Add project files.

---
+++ UploadFileApiDemo/Controllers/RobotController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UploadFileApiDemo.Controllers
+{
+    //[ApiController]
+    [Route("api/[controller]")]
+    public class RobotController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file,[FromServices] IWebHostEnvironment env)
+        {
+            var filePath = Path.Combine(env.ContentRootPath, "Files", file.FileName);
+            using var stream = System.IO.File.Create(filePath);
+            stream.Position = 0;
+            await file.CopyToAsync(stream);
+
+            return Ok();
+        }
+    }
+}
---
+++ UploadFileApp/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Net.Http.Headers;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Press any key to start upload ...");
+Console.ReadLine();
+var file = File.ReadAllBytes(@"F:\flower.jpg");
+
+await UploadImage("http://localhost:5291/api/robot", file);
+
+
+
+async Task UploadImage(string url, byte[] ImageData)
+{
+    var client=new HttpClient();
+
+    var requestContent = new MultipartFormDataContent();
+    var imageContent = new ByteArrayContent(ImageData);
+    imageContent.Headers.ContentType =
+        MediaTypeHeaderValue.Parse("image/jpeg");
+
+    requestContent.Add(imageContent, "file", "my-flower.jpg");
+
+    await client.PostAsync(url, requestContent);
+
+    
+}
